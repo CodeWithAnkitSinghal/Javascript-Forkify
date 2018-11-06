7e5bec577e70e98760e2b8b786e8a25a738ce5fc
Ankit Singhal <singhalankit769@gmail.com>
2018-11-06 20:30:34 +0530

    Javascript forkify app

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+//key1 668aa2a27d7dee3b45a011bec7f55d97
+//key2 70b8f1650b58a5fa3ca2e6de12026e62
+//key3 51571881e16b505cdae88ee715d02b41
+
+export const key = '668aa2a27d7dee3b45a011bec7f55d97'
+export const proxy = 'https://cors-anywhere.herokuapp.com/';
